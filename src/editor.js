289d881b32cbac67ddda0019a5beafbcc3fdcd4e
Christopher Jacobs <apexearth@gmail.com>
2017-11-11 23:37:54 -0800

    Draw layout to canvas and present in index.html. Add build/ to .gitignore + remove those files...

--- src/editor.js
+++ src/editor.js
@@ -1,22 +1,53 @@
+const Layout = require('@apexearth/layout')
+const draw   = require('@apexearth/layout-draw')
 require('./editor.less')
 const React = require('react')
 
 class Editor extends React.Component {
-    constructor(...args) {
-        super(...args)
+    constructor(props) {
+        super(props)
+        this.state = {
+            layout: props.layout
+        }
+    }
+
+    componentDidMount() {
+        if (!this.state.layout) {
+            this.new()
+        }
     }
 
     render() {
+        if (!this.layout) return null
+        setImmediate(() => {
+            draw(this.layout, {
+                canvas: this.refs.canvas,
+                scaleX: 25,
+                scaleY: 25,
+            })
+        }) // Hacky way to do this o_0
         return (
             <div>
-                Test
+                <canvas ref="canvas"/>
             </div>
         )
     }
 
+    get layout() {
+        return this.state.layout
+    }
+
+    new() {
+        const layout = new Layout()
+        layout.addSection(0, 0, 0, 0)
+        layout.addSection(0, 1, 0, 10)
+        this.setState({layout})
+    }
+
     static mount(element) {
         const ReactDOM = require('react-dom')
-        ReactDOM.render(<Editor/>, element)
+        const editor   = <Editor/>
+        ReactDOM.render(editor, element)
     }
 }
 
