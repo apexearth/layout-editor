8ca947dc8c1fa3b54d1ff072d5f1437a6abd912f
Christopher Jacobs <apexearth@gmail.com>
2017-11-13 00:42:57 -0800

    Allow click to add sections functionality, some bugs...

--- src/editor.js
+++ src/editor.js
@@ -1,14 +1,71 @@
 const Layout = require('@apexearth/layout')
 const draw   = require('@apexearth/layout-draw')
+const offset = require('mouse-event-offset')
 require('./editor.less')
 const React = require('react')
 
 class Editor extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {
-            layout: props.layout
+        this.state       = {
+            layout      : props.layout,
+            scale       : 25,
+            mouseDown   : null,
+            mouseCurrent: null,
         }
+        this.onMouseDown = event => {
+            let coord = this.coordinates(event)
+            this.setState({
+                mouseDown   : coord,
+                mouseCurrent: coord,
+            })
+            this.createSection(coord, coord)
+        }
+        this.onMouseMove = event => {
+            if (!this.state.mouseDown) return
+            let coord = this.coordinates(event)
+            if (coord[0] !== this.state.mouseCurrent[0] ||
+                coord[1] !== this.state.mouseCurrent[1]) {
+                this.setState({
+                    mouseCurrent: coord
+                })
+                this.createSection(this.state.mouseDown, coord)
+            }
+        }
+        this.onMouseUp   = event => {
+            if (!this.state.mouseDown) return
+            this.setState({
+                mouseDown   : null,
+                mouseCurrent: null,
+                section     : null,
+            })
+        }
+    }
+
+    get layout() {
+        return this.state.layout
+    }
+
+    coordinates(event) {
+        let coord = offset(event, this.refs.canvas)
+        coord[0]  = Math.floor(coord[0] / this.state.scale) + this.layout.bounds.left
+        coord[1]  = Math.floor(coord[1] / this.state.scale) + this.layout.bounds.top
+        return coord
+    }
+
+    createSection(mouseDown, mouseCurrent) {
+        if (this.state.section) {
+            this.state.section.remove()
+        }
+        let bounds = {
+            left  : Math.min(mouseDown[0], mouseCurrent[0]),
+            top   : Math.min(mouseDown[1], mouseCurrent[1]),
+            right : Math.max(mouseDown[0], mouseCurrent[0]),
+            bottom: Math.max(mouseDown[1], mouseCurrent[1]),
+        }
+        this.setState({
+            section: this.layout.add(bounds)
+        })
     }
 
     componentDidMount() {
@@ -19,22 +76,22 @@ class Editor extends React.Component {
 
     render() {
         if (!this.layout) return null
+        // Hacky way to do this o_0
         setImmediate(() => {
             draw(this.layout, {
                 canvas: this.refs.canvas,
-                scaleX: 25,
-                scaleY: 25,
+                scaleX: this.state.scale,
+                scaleY: this.state.scale,
             })
-        }) // Hacky way to do this o_0
+        })
         return (
-            <div>
+            <div className="layout-editor"
+                 onMouseDown={this.onMouseDown}
+                 onMouseMove={this.onMouseMove}
+                 onMouseUp={this.onMouseUp}>
                 <canvas ref="canvas"/>
             </div>
         )
-    }
-
-    get layout() {
-        return this.state.layout
     }
 
     new() {
